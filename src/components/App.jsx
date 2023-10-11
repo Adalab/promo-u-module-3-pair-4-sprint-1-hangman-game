d9b18e9296ed30272c5b0df66949ff039790b0b1
virchaca <you@example.com>
2023-10-11 11:32:21 +0200

    array word

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,73 @@
 // import viteLogo from '/vite.svg'
 import "../styles/App.scss";
-import "../fonts/KgTenThousandReasons-R1ll.ttf"
+import "../fonts/KgTenThousandReasons-R1ll.ttf";
 import { useState } from "react";
 
 function App() {
-  const [numberOfErrors, setNumberOfErrors] = useState (0);
-  const [lastLetter, setLastLetter] = useState ('');
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [lastLetter, setLastLetter] = useState("");
+  const [word, setWord] = useState("pepino");
+  const [userLetters, setUserLetters] = useState([]);
+
+
   const handleClick = () => {
-    setNumberOfErrors (numberOfErrors + 1);
-  }
+    setNumberOfErrors(numberOfErrors + 1);
+  };
+
   const handleLetter = (ev) => {
-    ev.preventDefault ();
+    ev.preventDefault();
     const letterPress = ev.key;
-    const regex = /^[a-z]+$/;
-    if(regex.test(letterPress)){
-    setLastLetter(letterPress);
-    } else if (letterPress === 'Backspace' || letterPress === ' ' || letterPress === 'Enter') {
-     setLastLetter(''); 
+    const regex = /^[a-z]+$/; 
+    
+    if (regex.test(letterPress)) {
+      setLastLetter(letterPress);
+      
+    } else if (
+      letterPress === "Backspace" ||
+      letterPress === " " ||
+      letterPress === "Enter"
+    ) {
+      setLastLetter("");
     }
-  }
+    setUserLetters([...userLetters, letterPress]);
+      console.log(userLetters);
+  };
+
+
   const handleSubmit = (ev) => {
     ev.preventDefault();
-  }
+  };
+
+  
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split("");
+    return wordLetters.map((eachLetter, index) => {
+      return <li key={index} className="letter"></li>;
+    });
+  };
+
+  // function dividirCadena(cadenaADividir, separador) {
+  //   var arrayDeCadenas = cadenaADividir.split(separador);
+  //   document.write('<p>La cadena original es: "' + cadenaADividir + '"');
+  //   document.write('<br>El separador es: "' + separador + '"');
+  //   document.write(
+  //     "<br>El array tiene " + arrayDeCadenas.length + " elementos: ",
+  //   );
+
+  //   for (var i = 0; i < arrayDeCadenas.length; i++) {
+  //     document.write(arrayDeCadenas[i] + " / ");
+  //   }
+  // }
+
+  // var cadenaVerso = "Oh brave new world that has such people in it.";
+  // var cadenaMeses = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec";
+
+  // var espacio = " ";
+  // var coma = ",";
+
+  // dividirCadena(cadenaVerso, espacio);
+  // dividirCadena(cadenaVerso);
+  // dividirCadena(cadenaMeses, coma);
 
   return (
     <div className="page">
@@ -33,7 +79,8 @@ function App() {
           <div className="solution">
             <h2 className="title">Solución:</h2>
             <ul className="letters">
-              <li className="letter">k</li>
+              {renderSolutionLetters()}
+              {/* <li className="letter">k</li>
               <li className="letter">a</li>
               <li className="letter"></li>
               <li className="letter">a</li>
@@ -42,7 +89,7 @@ function App() {
               <li className="letter"></li>
               <li className="letter">k</li>
               <li className="letter">e</li>
-              <li className="letter">r</li>
+              <li className="letter">r</li> */}
             </ul>
           </div>
           <div className="error">
@@ -66,12 +113,12 @@ function App() {
               type="text"
               name="last-letter"
               id="last-letter"
-              value = {lastLetter}
-              onKeyUp={handleLetter}
+              value={lastLetter}
+              onChange={handleLetter}
             />
           </form>
         </section>
-        <section className= {`dummy error-${numberOfErrors}`}>
+        <section className={`dummy error-${numberOfErrors}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
@@ -86,7 +133,9 @@ function App() {
           <span className="error-2 line"></span>
           <span className="error-1 line"></span>
         </section>
-        <button className="btn" onClick={handleClick}>Incrementar</button>
+        <button className="btn" onClick={handleClick}>
+          Incrementar
+        </button>
       </main>
     </div>
   );
